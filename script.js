e5883558d19aad7c0253d4c9514e0386c59c9b46
roger chahoud <roger_chahoud@outlook.com>
2024-04-24 00:13:16 -0300

    Calling the execution of setGrid()

--- script.js
+++ script.js
@@ -2,14 +2,18 @@ const mainDiv = document.querySelector(".main");
 
 let gridNumber = 16;
 
-for (let i = 0; i < gridNumber; i++) {
-  const parentGrid = document.createElement("div");
-  parentGrid.classList.add("parentGrid");
-  mainDiv.appendChild(parentGrid);
+function setGrid(gridNumber) {
+  for (let i = 0; i < gridNumber; i++) {
+    const parentGrid = document.createElement("div");
+    parentGrid.classList.add("parentGrid");
+    mainDiv.appendChild(parentGrid);
 
-  for (let j = 0; j < gridNumber; j++) {
-    const childGrid = document.createElement("div");
-    childGrid.classList.add("childGrid");
-    parentGrid.appendChild(childGrid);
+    for (let j = 0; j < gridNumber; j++) {
+      const childGrid = document.createElement("div");
+      childGrid.classList.add("childGrid");
+      parentGrid.appendChild(childGrid);
+    }
   }
 }
+
+setGrid(20);
